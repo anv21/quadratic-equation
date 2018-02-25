fa0dbbb0387e519533a5fc3f04bba196b04b1c3c
Anastasia <novakanastasya@yandex.ru>
2018-02-25 17:59:49 +0300

    Implementation of solveEquation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    const array = equation.split(' ');
+    const a = array[0];
+    const b = parseInt(array[3] + array[4], 10);
+    const c = parseInt(array[7] + array[8], 10);
+    const discriminant = Math.sqrt((Math.pow(b, 2) - 4 * a * c));
+
+    const x1 = Math.round((- b - discriminant) / (2 * a));
+    const x2 = Math.round((- b + discriminant) / (2 * a));
+
+    return [x1, x2].sort((a, b) => a - b);
+};
+
+
+
+
